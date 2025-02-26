b26f19c3f23907e61c775fa0be85bf849e0e6d4b
Hadid20 <gustihadid56@gmail.com>
2025-02-26 14:43:51 +0700

    update 9

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ wss.on("connection", function connection(ws) {
 
       if (data.event === "move") {
         console.log(
-          `Player ${data.player} moved to X: ${data.x}, Y: ${data.y}`
+          `Player ${data.player} moved to X: ${data.x} with message: ${data.message}`
         );
 
-        // Kirim posisi ke semua pemain lain
+        // Kirim posisi X, pesan, dan ID player ke semua pemain lain
         wss.clients.forEach((client) => {
           if (client !== ws && client.readyState === WebSocket.OPEN) {
             client.send(
@@ -21,14 +21,14 @@ wss.on("connection", function connection(ws) {
                 event: "update_position",
                 player: data.player,
                 x: data.x,
-                y: data.y,
+                message: data.message,
               })
             );
           }
         });
       }
     } catch (error) {
-      console.log("Invalid JSON data:", message);
+      console.log("Invalid JSON data:", message.toString());
     }
   });
 });
